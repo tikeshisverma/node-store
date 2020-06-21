a59325edec1c61c844934d866325dd0fce59c5b0
tikeshisverma <54203846+tikeshisverma@users.noreply.github.com>
2020-06-21 20:50:53 +0530

    added get api

--- index.js
+++ index.js
@@ -4,6 +4,30 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 const mongo =require('mongodb');
+app.get('/api/store', (req, res)=>{
+    console.log(req.query.id)
+    const mongoClient = mongo.MongoClient;
+    const url = 'mongodb://localhost'
+    mongoClient.connect(url, (err, client)=>{
+        if(err){
+            console.log(err)
+        }
+        else{
+            //console.log(db)
+            const db = client.db('storeDB');
+            const collection = db.collection('store')
+            collection.find({id:req.query.id}).toArray((err, result)=>{
+                if(err){
+                    console.log(err)
+                }
+                else{
+                    res.send({result})
+                    console.log('dqwfasdf',result)
+                }
+            })
+        }
+    })
+})
 app.post('/api/store' , (req, res) =>{
     console.log(req.body)
     const store ={
